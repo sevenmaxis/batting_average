efcd25600a6e68d28a29353d85647a07853729a9
Serhii Hopkalo <Sergey Gopkalo@gmail.com>
2020-07-22 08:32:46 +0300

    Add quering averages

--- spec/models/average_spec.rb
+++ spec/models/average_spec.rb
@@ -1,41 +1,50 @@
-require 'rails_helper'
-require 'csv'
+require "rails_helper"
 
 RSpec.describe Average, type: :model do
-  let(:build_averages) { 2.times.map { build(:average) } }
+  let(:build_averages) { 3.times.map { build(:average) } }
+  let(:averages) { 3.times.map { create(:average) } }
+
+  describe "Quering" do
+    it "finds specified records" do
+      average = averages.last
+
+      record = Average.where(year: average.year).where("teams @> ARRAY[?]::varchar[]", [average.teams.first]).first
+
+      expect(record.year).to eq(average.year)
+      expect(record.teams).to eq(average.teams)
+    end
+  end
 
   describe "Checks database restrictions" do
-    it 'does not create with same id' do
+    it "does not create with same id" do
       expect {
         build_averages.each { |average| average.id = "PS1" }.map(&:save!)
       }.to raise_error(ActiveRecord::RecordNotUnique)
     end
   end
 
-  describe 'Using indexs' do
+  describe "Using indexs" do
     before(:each) do
-      build_averages.map(&:save!)
-
       # NOTE: Force to use index for small amount of records
-      ActiveRecord::Base.connection.execute('SET enable_seqscan = OFF')
+      ActiveRecord::Base.connection.execute("SET enable_seqscan = OFF")
     end
 
-    it 'makes sure using index for year' do
-      analyze = Average.where(year: '1900').analyze
+    it "makes sure using index for year" do
+      analyze = Average.where(year: "1900").analyze
 
-      expect(analyze).to include('Bitmap Index Scan on index_averages_on_year_and_teams')
+      expect(analyze).to include("Bitmap Index Scan on index_averages_on_year_and_teams")
     end
 
-    it 'makes sure using index for teams' do
-      analyze = Average.where("teams @> ARRAY[?]::varchar[]", Average.last.teams).analyze
+    it "makes sure using index for teams" do
+      analyze = Average.where("teams @> ARRAY[?]::varchar[]", averages.last.teams).analyze
 
-      expect(analyze).to include('Bitmap Index Scan on index_averages_on_year_and_teams')
+      expect(analyze).to include("Bitmap Index Scan on index_averages_on_year_and_teams")
     end
 
-    it 'makes sure using index for year and teams' do
-      analyze = Average.where(year: '1891').where("teams @> ARRAY[?]::varchar[]", Average.last.teams).analyze
+    it "makes sure using index for year and teams" do
+      analyze = Average.where(year: "1891").where("teams @> ARRAY[?]::varchar[]", averages.last.teams).analyze
 
-      expect(analyze).to include('Bitmap Index Scan on index_averages_on_year_and_teams')
+      expect(analyze).to include("Bitmap Index Scan on index_averages_on_year_and_teams")
     end
   end
 end
